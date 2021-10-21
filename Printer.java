2dc0e1d402efbe4206b50420ad38fb67501d3354
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2021-10-21 19:17:23 -0400

    Update Printer.java
    Original printer java had no code in it because I was wasting too much time being inefficient in the main method.

--- Printer.java
+++ Printer.java
@@ -1,21 +1,22 @@
-//Printer.java
 
-/* This is a java file 'stub' or piece of a file designed to get you started on 
-* learning to code in java, specifically with multiple classes and methods that rely on the other.
-*
-*For this assignment, I would like you to fork, modify and commit this project, while
-* adding the following elements to your code.
-*
-* The receipt printer file could take arguments or parametrs from another class
-* This file will be purely for output
-****************************************
-* 40 characters is a standard width    * 
-* for typical receipt paper, your task *
-* is to make output from the screen    *
-* look like it is printing to a small  *
-* receipt type printer, with all the   *
-* text formatted to 40 characters wide *
-*                                      *
-* Have fun with the assignment!!       *
-****************************************
-*/
+import java.util.Scanner;
+
+public class Printer
+{
+   Kiosk movie = new Kiosk();
+   public void printreceipt()
+   {
+      System.out.println("\t **************************************** ");
+      System.out.print("\n");
+      System.out.println("\t \t" + " " +"Walmart of West Chester Ohio");
+      System.out.print("\n");
+
+      System.out.println("\t Qty \t Items \t \t" + " " + "\t Price");
+      System.out.print("\t **************************************** ");
+      
+
+
+   }
+
+
+}
