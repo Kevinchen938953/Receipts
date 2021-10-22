5f821c7a991079225b4c8609519e587bb00d86e0
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2021-10-21 21:32:37 -0400

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 public class Printer
 {
    Kiosk movie = new Kiosk();
-   public void printreceipt()
+   public void printreceipt()//method prints out header, qty, items, and price section 
    {
       System.out.println("\t **************************************** ");
       System.out.print("\n");
-      System.out.println("\t \t" + " " +"Walmart of West Chester Ohio");
+      System.out.println("\t \t" + " " +"Walmart of West Chester Ohio"); 
       System.out.print("\n");
 
       System.out.println("\t Qty \t Items \t \t" + " " + "\t Price");
       System.out.println("")
       System.out.print("\t **************************************** ");
+      //next steps involve carrying over variables from the Kiosk file putting those into the printer file
       
 
      }
