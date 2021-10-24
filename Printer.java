fdc9799b0cbc6c136e640d27be7054d9bfcc59e2
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2021-10-24 12:03:05 -0400

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -6,27 +6,19 @@ public class Printer
    private int[] qty;
    private String[] items;
    private double tax = 0.07;
-   private double price = 1.25;
-   private double price2= 2.2;
-   private double price3 = 3.3;
-   private double price4 = 4.4;
-   private double price5=5.5;
+   private double[] price;
    private double subtotal;
    private double total;
-   public Printer(int[] quantity, String[] stuffname, double Ttax, double Price, double Price2, double Price3, double Price4, double Price5, double Total, double Subtotal)
+   public Printer(int[] quantity, String[] stuffname, double Ttax, double[] Price, double Total, double Subtotal)
    {
       qty=quantity;
       items = stuffname;
       tax = Ttax;
       price = Price;
-      price2 = Price2;
-      price3 = Price3;
-      price4 = Price4;
-      price5 = Price5;
       total = Total;
       subtotal = Subtotal;
       
-   }
+   }//Constructs variables from Kiosk java
    
    public void printreceipt()//method prints out header, qty, items, and price section 
    {
@@ -38,9 +30,14 @@ public class Printer
 
       System.out.println("\t Qty \t Items \t \t" + " " + "\t Price");
       for(int i =0; i<5; i++)
-      {
-         System.out.println("\t" + qty[i] + "\t" + items[i]);
+      {S
+         System.out.println("\t" + qty[i] + "\t" + items[i] + "\t" + "\t" + price[i]*qty[i]);//Prints out the amount of items bought, the item name, and the item subtotal
       }
+      System.out.print("\n");
+      System.out.println("\t" + "Subtotal: " + " " +subtotal);
+      System.out.println("\t" + "Total is " + " " + total);//Returns total
+
+
       System.out.print("\t **************************************** ");
       //next steps involve carrying over variables from the Kiosk file putting those into the printer file
       
